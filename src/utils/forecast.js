8626de9c0f8da2b2cf4f89b8976cd198bfbbce07
sikandermerchant <sikander.merchant@gmail.com>
2020-08-12 17:44:11 +0100

    Removed global dependency of nodemon

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,12 @@
 const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=6356a94b892b52ec7d337500172528cc&query=${latitude},${longitude}`;
-  request({
+  request(
+    {
       url,
       json: true,
     },
-    (error, {
-      body
-    } = {}) => {
+    (error, { body } = {}) => {
       if (error) {
         callback("Unable to connect to the weather api service", undefined);
       } else if (body.error) {
@@ -16,9 +15,12 @@ const forecast = (latitude, longitude, callback) => {
           undefined
         );
       } else {
-        callback(undefined, `It is currently ${body.current.weather_descriptions[0]} in ${body.location.name}, ${body.location.region}.
+        callback(
+          undefined,
+          `It is currently ${body.current.weather_descriptions[0]} in ${body.location.name}, ${body.location.region}.
         The current temperature is ${body.current.temperature} degrees; although it feels like ${body.current.feelslike} degrees.
-        The humidity is ${body.current.humidity}%`)
+        The humidity is ${body.current.humidity}%.`
+        );
       }
     }
   );
